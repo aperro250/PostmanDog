82865576671ba6d6f88fe6cd124d5a7bbc3a5a6b
Aperro <97552699+aperro250@users.noreply.github.com>
2026-01-27 12:02:35 +0100

    Modified create_config.py

--- script/create_config.py
+++ script/create_config.py
@@ -4,6 +4,11 @@ import clear
 
 # Store the path one level up for future creation of "config.json"
 base_dir = os.path.dirname(os.path.abspath(os.path.join(__file__, '..')))
+# Store the config derectory
+config_path = os.path.join( base_dir,"config.json")
+
+# Create a chose variable for the future work
+choice = ""
 
 # Creating a module of config sintaxis
 
@@ -26,6 +31,7 @@ config_model = {
     "Telegram": {
             "group1": {
                 # The telegram group id
+                "name":"",
                 "id": "",
 
                 "channel1": {
@@ -42,6 +48,29 @@ config_model = {
 
 }
 
+
+print(config_path)
+# Create a config.sjon file if it don't exists
+if not os.path.exists(config_path):
+	
+	print ("creating new config file")
+	with open(config_path, "w", encoding="utf-8") as f:
+		json.dump(config_model, f, indent=4)
+	
+
+
+# Create a variable for work with config
+with open(config_path, "r", encoding="utf-8") as f:
+
+	config = json.load(f)
+
+# Update the config. Writes the local variable "config" into the config.json
+def update_config():
+	with open (config_path, "w", encoding="utf-8", ensure_ascci=False) as f:
+		json.dump(config, f, indent=4)
+
+
+# ~~~~~~~~~~~~~~~~ GUI ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~	
 def createConfig ():
 
     print ("Welcome to the config creation assistant")
@@ -67,11 +96,49 @@ def createConfig ():
         else:
             print ("You have introduced the incorect number, please try again")
 
+
+# ........................ TELEGRAM ...............................
 def telegram_config ():
     clear.clear()
+    
+    telegram = config.get("Telegram")
     print("Telegram_config")
-    input()
+    
+    print("""
+    What you want to do?
+    1: Add a new group
+    """)
+    # If there are an id for the first group:
+    if "id" in telegram.get("group") != "" :
+    
+    	print("""
+    	2: Modife an excisting Telegram group
+	3: Add a new channel
+    	""")
+    	
+    choice = input()
+    
+    if choise == 1:
+    	add_telegram_group()
+    elif choice == 2:
+    	modife_telegram_group()
+    elif choice == 3:
+    	add_telegram_channel()
+    else:
+    	print ("You have introduced the incorect number, please try again")
+    
 
+def add_telegram_group ():
+	input ()
+	
+def modife_telegram_group ():
+	input ()
+	
+def add_telegram_channel ():
+	input ()
+
+
+# ......................DISCORD ........................
 def discord_config ():
     clear.clear()
     print("Discord_confid")
